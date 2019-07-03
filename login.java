e52fa6cbb243ebba5d9580987830f9d25f6d8142
ghemar <girish0773@gmail.com>
2019-07-04 00:57:16 +0530

    b=20

--- login.java
+++ login.java
@@ -1,19 +1,9 @@
 public class login
 {
      public static void main(string arg[]) 
-      lksdf;k
+      int b="50"
       {
-<<<<<<< HEAD
-     iasfafafasas
-      dg
-
-      dg
-      sdg
-
-=======
-     dsfsdf
-	     dfdf
-      }
->>>>>>> 9e2578e35c8ddfb38a4afe902cd48f9af780cfc9
+        print ln "b"
+ }
 
 }
